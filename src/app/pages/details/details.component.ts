165e0d73d353c66b7d19f11bb3dde81cf9f33104
Antonio Alves <antoniocunha9610@gmail.com>
2023-05-24 20:26:03 -0300

    Implementando compenente Details PART-1

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon:String='https://pokeapi.co/api/v2/pokemon'
+  private urlName:String='https://pokeapi.co/api/v2/pokemon-species'
+  //pegar o parametro passado pela url
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ){}
 
+  ngOnInit(): void {
+    this.pokemon;
+  }
+  get pokemon(){
+    const id = this.activatedRoute.snapshot.params['id'];
+    const pokemon= this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+
+    const name=this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+    //O forkJoin faz as buscas e dps retorna respostas
+    //de pokemon e name, economiza o subscribe
+    return forkJoin([ pokemon, name]).subscribe(
+      res=> {
+        console.log(res);
+      }
+    )
+
+  }
 }
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,7 +7,7 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent {
-
+  private setAllPokemons:any
   public getAllPokemons:any
   constructor(private pokeApiService: PokeApiService){
 
@@ -16,8 +16,21 @@ export class PokeListComponent {
   ngOnInit(): void{
      this.pokeApiService.apiListALLPokemons.subscribe(
       res=> {
-        this.getAllPokemons=res.results;
+        this.setAllPokemons=res.results
+        this.getAllPokemons=this.setAllPokemons;
+
       }
     )
+  }
+  //regastar do emmiter
+  public getSearch(value:string){
+
+
+    const filter = this.setAllPokemons.filter((res:any)=>{
+      return !res.name.indexOf(value.toLowerCase());
+
+    });
+    this.getAllPokemons=filter;
+
   }
 }
